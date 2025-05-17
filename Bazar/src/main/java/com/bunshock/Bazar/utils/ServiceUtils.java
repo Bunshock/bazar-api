985d4e89f8a938d1515fd9ad5c49e111e5327650
Bunshock <leomanto98@gmail.com>
2025-05-17 04:22:38 -0300

    Se corrigió la correcta visualización de los datos de Venta, quitando productos duplicados y corrigiendo el conteo de productos.

--- Bazar/src/main/java/com/bunshock/Bazar/utils/ServiceUtils.java
+++ Bazar/src/main/java/com/bunshock/Bazar/utils/ServiceUtils.java
@@ -2,8 +2,11 @@ package com.bunshock.Bazar.utils;
 
 import com.bunshock.Bazar.dto.ProductoVentaDTO;
 import com.bunshock.Bazar.dto.VentaMostrarDTO;
+import com.bunshock.Bazar.model.Producto;
 import com.bunshock.Bazar.model.Venta;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
@@ -15,7 +18,10 @@ public class ServiceUtils implements IServiceUtils {
     @Override
     public VentaMostrarDTO mapVentaToVentaMostrarDTO(Venta venta) {
         
-        // Para mostrar la cantidad de cada producto, necesitamos contar
+        // Como muestro la cantidad de cada producto comprado, elimino duplicados
+        Set<Producto> productosUnicos = new HashSet<>(venta.getListaProductos());
+
+        // Para mostrar la cantidad de cada producto, necesito contar
         Map<Long, Long> cantidadesPorId = venta.getListaProductos().stream()
                 .map(producto -> producto.getCodigo_producto())
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
@@ -24,7 +30,7 @@ public class ServiceUtils implements IServiceUtils {
                 venta.getFecha_venta(),
                 venta.getTotal(),
                 venta.isRealizada(),
-                venta.getListaProductos().stream()
+                productosUnicos.stream()
                         .map(producto -> new ProductoVentaDTO(
                                 producto.getNombre(),
                                 producto.getMarca(),
