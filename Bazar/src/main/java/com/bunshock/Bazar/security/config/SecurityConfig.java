dbc24a7d93a8b5e36571b8644c259c2b2657152d
Bunshock <leomanto98@gmail.com>
2025-05-17 20:31:11 -0300

    Se hizo público el endpoint: /productos/{product_code}

--- Bazar/src/main/java/com/bunshock/Bazar/security/config/SecurityConfig.java
+++ Bazar/src/main/java/com/bunshock/Bazar/security/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
 
 @Configuration
@@ -43,8 +44,9 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         // Acceso global a registro y login
                         .requestMatchers("/auth/**").permitAll()
-                        // Acceso global a la lista de productos del bazar
+                        // Acceso global a la lista de productos del bazar, y a productos específicos
                         .requestMatchers(HttpMethod.GET, "/productos").permitAll()
+                        .requestMatchers(new RegexRequestMatcher("/productos/\\d+$", "GET")).permitAll()
                         // Cualquier otro endpoint, necesita autenticación
                         .anyRequest().authenticated()
                 )
