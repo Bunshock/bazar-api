f9bf67a85a28b5803cca7faebfe5b2ee2158c403
Bunshock <leomanto98@gmail.com>
2025-05-20 23:35:03 -0300

    Configuración de 'timestamp' para incluir la hora en la respuesta HTTP de la API

--- Bazar/src/main/java/com/bunshock/Bazar/dto/ApiResponseDTO.java
+++ Bazar/src/main/java/com/bunshock/Bazar/dto/ApiResponseDTO.java
@@ -1,5 +1,6 @@
 package com.bunshock.Bazar.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,11 +12,13 @@ import lombok.experimental.SuperBuilder;
 public abstract class ApiResponseDTO {
     
     private int status;
+    
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime timestamp;
-
+    
     public ApiResponseDTO() {
     }
-
+    
     public ApiResponseDTO(int status, LocalDateTime timestamp) {
         this.status = status;
         this.timestamp = timestamp;
